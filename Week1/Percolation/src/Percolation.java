5ef517559c32654a5c04811d8731db5588fd51cc
Rodrigo Villarreal <noiarek@gmail.com>
2018-11-05 22:04:44 -0600

    Updating code to use QuickUnion

--- Week1/Percolation/src/Percolation.java
+++ Week1/Percolation/src/Percolation.java
@@ -1,55 +1,108 @@
+import java.util.Scanner;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 	
 	public static final boolean OPEN = true;
 	public static final boolean CLOSED = false;
 	
-	public int[][] sites;
+	public int sitesOpened;
+	public int top;
+	public int bottom;
+	
 	public boolean[][] open;
+
+	public WeightedQuickUnionUF wqfu;
 	
 	public Percolation(int n) {
-		int v = 0;
-		sites = new int[n][n];
+		
+		if(n < 0) {
+			throw new IllegalArgumentException("Invalid argument n");
+		}
+		
+		top = 0;
+		bottom = n * n + 1;
+		
 		open = new boolean[n][n];
 		
 		for(int i = 0; i < n ; i++) {
 		   for(int j = 0; j < n; j++) {
-			   sites[i][j] = v++;
 			   open[i][j] = CLOSED;
 		   	}
 		}
 	}
+	
+	protected int validateAndDecrease(int p) {
+		if (p <= 0 || p > open.length) {
+			throw new IndexOutOfBoundsException("row index i out of bounds");
+		}
+		
+		return p--;
+	}
+	
+	private int XYTo1d(int row, int col) {
+		   return (open.length * row) + 1;
+	}
    
-   public void open(int row, int col)
-   {
+   public void open(int row, int col) {
+	   row = validateAndDecrease(row);
+	   col = validateAndDecrease(col);
+	   
+	   if(open[row][col] == OPEN) {
+		   return;
+	   }
+	   
 	   open[row][col] = OPEN;
+	   sitesOpened++;
 	   
 	   // top
 	   if(row > 0 && open[row - 1][col] == OPEN) {
-		   sites[row-1][col] = sites[row][col]; 
+		   wqfu.union(XYTo1d(row, col), XYTo1d(row-1, col));
 	   }
 	   
 	   // left
 	   if(col > 0 && open[row][col - 1] == OPEN) {
-		   sites[row][col - 1] = sites[row][col]; 
+		   wqfu.union(XYTo1d(row, col), XYTo1d(row, col-1));
 	   }
 	   
 	   // right
-	   if(col < sites.length && open[row][col + 1] == OPEN) {
-		   sites[row][col + 1] = sites[row][col]; 
+	   if(col < open.length && open[row][col + 1] == OPEN) {
+		   wqfu.union(XYTo1d(row, col), XYTo1d(row, col + 1));
 	   }
 	   
 	   // down
-	   if(row < sites.length && open[row + 1][col] == OPEN) {
-		   sites[row + 1][col] = sites[row][col]; 
+	   if(row < open.length && open[row + 1][col] == OPEN) {
+		   wqfu.union(XYTo1d(row, col), XYTo1d(row + 1, col));
 	   }   
    }
+   
+   public boolean isOpen(int row, int col) {
+	   row = validateAndDecrease(row);
+	   col = validateAndDecrease(col);
+	   return open[row][col] == OPEN;
+   }
 	
-   /*public    void open(int row, int col)    // open site (row, col) if it is not open already
-   public boolean isOpen(int row, int col)  // is site (row, col) open?
-   public boolean isFull(int row, int col)  // is site (row, col) full?
-   public     int numberOfOpenSites()       // number of open sites
-   public boolean percolates()              // does the system percolate?
-
-   public static void main(String[] args)   // test client (optional)
-	*/
+   public int numberOfOpenSites() {
+	   return open.length * open.length - sitesOpened;
+   }
+   
+   public boolean percolates(){
+	   return wqfu.connected(top, bottom);
+   }
+   
+   public boolean isFull(int row, int col) {
+	   row = validateAndDecrease(row);
+	   col = validateAndDecrease(col);
+	   return wqfu.connected(top, bottom);
+   }
+   
+   public static void main(String[] args) {
+	   Percolation p = new Percolation(5);
+	   Scanner sc = new Scanner(System.in);
+	   while(true) {
+		   p.open(sc.nextInt(), sc.nextInt());
+		   System.out.println(p.percolates());
+	   }
+   }
 }
